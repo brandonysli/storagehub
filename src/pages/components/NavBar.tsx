501e12de86fd09920e777dafd62e2340e97144e8
Andrew Cheung <aec295@cornell.edu>
2023-02-14 18:12:01 -0500

    Set up basic NavBar component.

--- src/pages/components/NavBar.tsx
+++ src/pages/components/NavBar.tsx
@@ -1,3 +1,45 @@
 // NavBar props
 
 // what does the navbar need to show for example whether someone is logged in or not
+
+import React from "react";
+import styles from './NavBar.module.css';
+import * as data from "./links.json";
+
+const linksString = JSON.stringify(data);
+const links = JSON.parse(linksString).links;
+
+type Link = {
+    label:string;
+    href:string;
+}
+
+const Links: React.FC<{links: Link[]}> = ({links}) => {
+    return (
+        <div className = {styles['links-container']}>
+            {links.map((link:Link) => {
+                return (
+                    <div key = {link.href} className = {styles['link']}>
+                        <a href = {link.href}>
+                            {link.label}
+                        </a>
+                    </div>
+                )
+            })}
+        </div>
+    )
+}
+
+const NavBar: React.FC<{}> = () => {
+    return(
+       <nav className = {styles.navbar}>
+            <div className = {styles['logo-container']}>
+                <span>Logo</span>
+            </div>
+            <Links links = {links} />
+                
+        </nav>
+    )
+}
+
+export default NavBar;
