0de7f053cbaa0cfb9eb1280cab5cc942f366d100
Brandon Li <brandonyusenli@gmail.com>
2023-02-19 14:38:22 -0500

    schema changes

--- src/pages/api/schema/user/user.query.ts
+++ src/pages/api/schema/user/user.query.ts
@@ -4,12 +4,19 @@ import { prisma } from '../../prisma'
 builder.queryFields((t) => ({
   ManyUser: t.prismaField({
     type: ["User"],
-      nullable: true,
-      args: {},
-      resolve: (query, root, args) => 
-        prisma.user.findMany({
-          ...query
-        })
+    nullable: true,
+    args: {},
+    resolve: (query, root, args) => 
+      prisma.user.findMany({
+        ...query
+      })
   })
-})
-)
+}))
+
+builder.queryFields((t) => ({
+  OneUser: t.prismaField({
+    type: "User",
+
+  })
+}))
+
