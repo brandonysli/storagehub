66508ec35cc07dd72b70924bfbcbb146f6c38c8c
Dora Weng <doraweng@dhcp-vl2041-39980.eduroam.cornell.edu>
2023-02-19 17:39:33 -0500

    created queries for package

--- src/pages/api/schema/package/package.query.ts
+++ src/pages/api/schema/package/package.query.ts
@@ -1,1 +1,31 @@
 import { builder } from "../../builder"
+import { prisma } from '../../prisma'
+
+builder.queryFields((t) => ({
+    ManyPackage: t.prismaField({
+        type: ["Package"],
+        nullable: true,
+        args: {},
+        resolve: (query) => 
+        prisma.package.findMany({
+            ...query
+        })
+    }),
+
+    OnePackage: t.prismaField({
+        type : "Package",
+        nullable: true,
+        args: {
+            id: t.arg({ type: "String"})
+        },
+        resolve: (query, root, args) => 
+            prisma.package.findFirst({
+            ...query,
+            where: {
+                id: args.id || undefined, 
+            }
+
+        })
+    })
+})
+)
